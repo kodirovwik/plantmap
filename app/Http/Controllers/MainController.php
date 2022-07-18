ab2dd172e5a1c9e271af1009e3f2de70c0330bb2
TdX <udenkov.p@gazelkin.ru>
2022-07-18 14:37:00 +0300

    Убираем ненужные строчки

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -11,48 +11,4 @@ class MainController extends Controller
     {
         return 'Privet';
     }
-    public function index()
-    {
-        $i = 1;
-        $k = 1;
-        while ($i<=9){
-            while ($k<=$i){
-                echo '#';
-                $k++;
-            }
-            echo '<br>';
-            $i++;
-            $k = 1;
-        }
-        echo '<br><br>';
-        $i = 9;
-        $k = 9;
-        while ($i>0) {
-            while ($k>0){
-                echo '#';
-                $k--;
-            }
-            echo '<br>';
-            $i--;
-            $k=$i;
-        }
-        echo '<br><br>';
-        $i = 1;
-        $k = 1;
-        $j = 1;
-        while ($i<=9) {
-            while ($k<=9-$i){
-                echo "\t";
-                $k++;
-            }
-            while ($j<=$i){
-                echo '#';
-                $j++;
-            }
-            echo '<br>';
-            $i++;
-            $j = 1;
-            $k = 1;
-        }
-    }
 }
